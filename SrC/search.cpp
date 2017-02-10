6fb59d4dbd4795c08d11b6fffd4c36b3a5444e97
JoselitoPimPamFuego <quoboy_28@hotmail.com>
2017-02-10 08:36:22 +0100

    Update search.cpp

--- SrC/search.cpp
+++ SrC/search.cpp
@@ -1038,6 +1038,9 @@ moves_loop: // When in check search starts from here
               r = std::max(DEPTH_ZERO, (r / ONE_PLY - ss->history / 20000) * ONE_PLY);
           }
 
+          if (study && ss->ply < depth / 2 - ONE_PLY)
+              r = DEPTH_ZERO;
+	      
           Depth d = std::max(newDepth - r, ONE_PLY);
 
           value = -search<NonPV>(pos, ss+1, -(alpha+1), -alpha, d, true, false);
